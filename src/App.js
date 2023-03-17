e94765fa5444533b22f83346d56d3c7e5d33891e
Ticia Francisco <ticiadev@gmail.com>
2023-03-16 20:48:56 -0700

    Update with basic layout

--- src/App.js
+++ src/App.js
@@ -2,66 +2,77 @@ import './App.css';
 
 const App = () => {
   return (
-    <>
-      <Nav />
-    </>
+      <div className="drawer drawer-mobile">
+        <MainPanel />
+        <Navbar />
+      </div>
+  )
+}
+
+const Header = () => {
+  return (
+    <div className="w-full navbar bg-base-300">
+      <div className="flex-none lg:hidden">
+        <label htmlFor="my-drawer-2" className="btn btn-square btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        </label>
+      </div> 
+      <Title />
+    </div>
   )
 }
 
 const Title = () => {
   return (
+    <div className="flex-1 px-2 mx-2"><h2 className="prose md:prose-lg lg:prose-xl">Story Title</h2></div>
+  )
+}
+
+const MainPanel = () => {
+  return (
     <>
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" /> 
       <div className="drawer-content flex flex-col">
-        <div className="w-full navbar bg-base-300">
-          <div className="flex-none lg:hidden">
-            <label htmlFor="my-drawer-2" className="btn btn-square btn-ghost">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-            </label>
-          </div> 
-          <div className="flex-1 px-2 mx-2"><h2 className="prose md:prose-lg lg:prose-xl">Story Title</h2></div>
-        </div>
+        <Header />
         <Story />
       </div>
     </>
   )
 }
 
-const Nav = () => {
+const Navbar = () => {
   return (
-    <div className="drawer drawer-mobile">
-      <Title />
       <div className="drawer-side bg-base-300">
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
-        <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+        <ul className="menu p-4 w-80 bg-base-300 text-base-content">
           <h1 className="prose md:prose-lg lg:prose-xl">Story App</h1>
-          <NewStory />
+          <div className="my-1 mx-auto"><AddNew item="Story" /></div>
           <li><a>Sidebar Item 1</a></li>
           <li><a>Sidebar Item 2</a></li>
         </ul>
       </div>
-    </div>
   )
 }
 
-const NewStory = () => {
+const AddNew = ({item}) => {
   return (
-    <button className="btn btn-primary btn-sm" id="button--story-new">New Story</button>
+    <button className="btn btn-primary btn-sm">New {item}</button>
   )
 }
 
 const Story = () => {
   return (
-    <>
+    <section className="w-10/12 mt-8 flex flex-col justify-start items-center gap-4">
+      <AddNew item="Entry" />
       <Entry />
       <Entry />
-    </>
+    </section>
   )
 }
 
 const Entry = () => {
   return (
-    <div className="card w-96 bg-primary text-primary-content">
+    <div className="card lg:card-side bg-primary text-primary-content">
       <div className="card-body">
         <p>If a dog chews shoes whose shoes does he choose?</p>
       </div>
